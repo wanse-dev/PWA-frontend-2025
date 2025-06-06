1a78a38c3070c08d64b16c434cdcdc85357661a1
wanse-dev <wansedev@gmail.com>
2025-06-06 17:01:29 -0300

    created new demo component

---
+++ src/FirstComponent.tsx
@@ -0,0 +1,12 @@
+
+
+function FirstComponent() {
+    return (
+        <>
+            <h1>First component title</h1>
+            <p>Hello world!</p>
+        </>
+    )
+}
+
+export default FirstComponent;
