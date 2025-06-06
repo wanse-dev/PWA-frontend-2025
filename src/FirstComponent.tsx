7dd98bfd707b7239da6ddd716d80f4d9353c4aff
wanse-dev <wansedev@gmail.com>
2025-06-06 18:03:58 -0300

    updated first component, states applied

--- src/FirstComponent.tsx
+++ src/FirstComponent.tsx
@@ -1,12 +1,35 @@
-
+import { useState } from 'react';
+import './FirstComponent.css';
 
 function FirstComponent() {
-    return (
-        <>
-            <h1>First component title</h1>
-            <p>Hello world!</p>
-        </>
-    )
+  const [isAdmin, setAdmin] = useState(false);
+
+  const handleClick = () => {
+    setAdmin(isAdmin ? false : true);
+  };
+
+  return (
+    <>
+      <h1>First component title</h1>
+      <p>Hello world!</p>
+
+      <br />
+
+      <button
+        onClick={handleClick}
+        className={isAdmin ? 'admin-true' : 'admin-false'}
+      >
+        {isAdmin ? 'Disable admin' : 'Enable admin'}
+      </button>
+
+      <div>
+        ¿Is admin? {' '}
+        <span className={isAdmin ? 'admin-true' : 'admin-false'}>
+          {isAdmin ? 'Yes' : 'No'}
+        </span>
+      </div>
+    </>
+  );
 }
 
 export default FirstComponent;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import './index.css'
+import './index.css'
 import FirstComponent from './FirstComponent.tsx'
 
 createRoot(document.getElementById('root')!).render(
